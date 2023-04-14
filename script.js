40d54318640084071912f4ac0d9c4ea6964f46b6
Yoviar Pauzi <84509445+yoviarpauzi@users.noreply.github.com>
2023-04-14 14:54:44 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// change background
-let twitterIcon = document.querySelector("#twitter-icon");
-twitterIcon.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  document.querySelector(".twitter__container").classList.toggle("dark");
-});
